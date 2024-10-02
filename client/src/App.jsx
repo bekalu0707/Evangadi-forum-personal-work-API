55934f6f3816139f66674b82dfe095769fa9cfe0
Habtamu <73044474+habteprivate@users.noreply.github.com>
2024-10-01 20:16:42 -0400

    Front end Base Code

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Evangadi Fourm Project Group 2</h1>
+    </>
+  )
+}
+
+export default App
