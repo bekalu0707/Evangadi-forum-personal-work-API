74b1eb57ab956a93eb13a5e11ecedd58725e9207
BEKAML <bekalumsc@gmail.com>
2024-10-10 11:48:33 +0300

    database config modified for render

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -2,9 +2,9 @@ const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createConnection({
     // socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock", //path to mysql sock in MAMP
-    user: process.env.user,
+    user:process.env.user,
     database:process.env.database,
-    host:"localhost",
+    host:" sql12.freemysqlhosting.net",
     password:process.env.password
    
    
